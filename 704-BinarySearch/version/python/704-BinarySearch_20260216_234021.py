9ee8c9c9cfeeb60f9a25bf8341acfb3ef89985c2
jayanikrishna <148961826+jayanikrishna@users.noreply.github.com>
2026-02-16 23:40:22 +0530

    Create 704-BinarySearch

---
+++ 704-BinarySearch/version/python/704-BinarySearch_20260216_234021.py
@@ -0,0 +1,25 @@
+# Last updated: 2/16/2026, 11:40:21 PM
+1class Solution(object):
+2    def search(self, nums, target):
+3        """
+4        :type nums: List[int]
+5        :type target: int
+6        :rtype: int
+7        """
+8        left, right = 0, len(nums) - 1
+9        
+10        while left <= right:
+11            # Finding the middle index
+12            mid = left + (right - left) // 2
+13            
+14            if nums[mid] == target:
+15                return mid
+16            elif nums[mid] < target:
+17                # Target is in the right half, discard left half
+18                left = mid + 1
+19            else:
+20                # Target is in the left half, discard right half
+21                right = mid - 1
+22                
+23        # If we exit the loop, the target was not in the array
+24        return -1
