7558f8f47b098d16e10116bc32771da5846467cc
jayanikrishna <148961826+jayanikrishna@users.noreply.github.com>
2026-03-07 23:51:17 +0530

    File updated at 3/7/2026, 11:51:16 PM

--- 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.py
+++ 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.py
@@ -1,21 +1,16 @@
-# Last updated: 3/7/2026, 11:49:47 PM
+# Last updated: 3/7/2026, 11:51:16 PM
 1class Solution:
-2    def removeDuplicates(self, nums: list[int]) -> int:
-3        if not nums:
-4            return 0
+2    def removeElement(self, nums: list[int], val: int) -> int:
+3        # k keeps track of the position for the next valid element
+4        k = 0
 5        
-6        # 'i' is the pointer for the last unique element found
-7        i = 0
-8        
-9        # 'j' is the explorer pointer
-10        for j in range(1, len(nums)):
-11            # If we find a value different from our last unique value
-12            if nums[j] != nums[i]:
-13                # Move the unique pointer forward
-14                i += 1
-15                # Update the unique position with the new value
-16                nums[i] = nums[j]
-17        
-18        # Return the count of unique elements (index + 1)
-19        return i + 1
-20        
+6        for i in range(len(nums)):
+7            # If the current element is NOT the one we want to remove
+8            if nums[i] != val:
+9                # Move it to the front at position k
+10                nums[k] = nums[i]
+11                # Increment k to the next available slot
+12                k += 1
+13                
+14        # k now represents the number of elements not equal to val
+15        return k
