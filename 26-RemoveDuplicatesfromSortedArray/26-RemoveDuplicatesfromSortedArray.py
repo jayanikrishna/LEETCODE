6990c400aaab262022a2bb453ba6e5cd7ab57efa
jayanikrishna <148961826+jayanikrishna@users.noreply.github.com>
2026-03-07 23:52:26 +0530

    File updated at 3/7/2026, 11:52:24 PM

--- 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.py
+++ 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.py
@@ -1,16 +1,17 @@
-# Last updated: 3/7/2026, 11:51:16 PM
+# Last updated: 3/7/2026, 11:52:24 PM
 1class Solution:
-2    def removeElement(self, nums: list[int], val: int) -> int:
-3        # k keeps track of the position for the next valid element
-4        k = 0
-5        
-6        for i in range(len(nums)):
-7            # If the current element is NOT the one we want to remove
-8            if nums[i] != val:
-9                # Move it to the front at position k
-10                nums[k] = nums[i]
-11                # Increment k to the next available slot
-12                k += 1
-13                
-14        # k now represents the number of elements not equal to val
-15        return k
+2    def strStr(self, haystack: str, needle: str) -> int:
+3        n, h = len(needle), len(haystack)
+4        
+5        # If the needle is longer than the haystack, it can't be found
+6        if n > h:
+7            return -1
+8            
+9        # The loop range ensures we don't look past the end of the haystack
+10        # We stop at h - n + 1
+11        for i in range(h - n + 1):
+12            # Extract a substring of length n and compare
+13            if haystack[i:i + n] == needle:
+14                return i
+15                
+16        return -1
